7e72a3aaad01c11b68f76438d1df0fd197e12df2
Sowjan <sowjanya.sritharasarma@macaulay.cuny.edu>
2020-11-01 20:47:05 -0500

    added airbnb roomtype chart

--- ha_8.py
+++ ha_8.py
@@ -139,6 +139,17 @@ ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 fig1 = plt.gcf()
 fig1.savefig('roomTypesB.png')
 
+#CITY
+rooms = airbnb['room_type'].value_counts()
+explode = (0, 0, 0.1)
+
+fig1, ax1 = plt.subplots()
+ax1.pie(rooms, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+fig1 = plt.gcf()
+fig1.savefig('roomTypesA.png')
+
 #########################################################
 
 
